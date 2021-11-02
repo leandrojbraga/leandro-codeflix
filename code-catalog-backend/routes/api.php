55d0ebf0fe92b33adca97f4d1a1f3ef28c4d500c
leandrojbraga <leandroj.braga@gmail.com>
2021-11-01 23:08:44 -0300

    Unit tests

--- code-catalog-backend/routes/api.php
+++ code-catalog-backend/routes/api.php
@@ -18,10 +18,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::group(['namespace' => 'Api'], function () {
-    Route::resources([
-            'categories' => 'CategoryController',
-            'genres' => 'GenreController'
-        ],
-        ['except' => ['create', 'edit']]
-    );
+    $exceptCreateAndEdit = [
+        'except' => ['create', 'edit']
+    ];
+
+    Route::resource('categories', 'CategoryController', $exceptCreateAndEdit);
+    Route::resource('genres', 'GenreController', $exceptCreateAndEdit);
 });
